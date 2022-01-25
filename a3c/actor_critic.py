e75ded882838f5db0dd56a1731eb20ea5e8c73ae
Mark Trovinger <mark.trovinger@gmail.com>
2022-01-25 15:46:18 -0500

    added gru to network

--- a3c/actor_critic.py
+++ a3c/actor_critic.py
@@ -6,11 +6,18 @@ import torch.nn.functional as F
 from torch.distributions import Categorical
 
 class ActorCritic(nn.Module):
-    def __init__(self, gamma=0.99, beta=0.01, hidden_units=256):
+    def __init__(self, input_dims, gamma=0.99, beta=0.01, hidden_units=256):
+        
+        super(ActorCritic, self).__init__()
 
-        self.conv1 = nn.Conv2d(input.shape, 32, kernel_size=(3,3), stride=2,
+        self.conv1 = nn.Conv2d(input_dims[0], 32, kernel_size=(3,3), stride=2,
                 padding=1)
         self.conv2 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1)
 
         self.conv4 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1)
+
+        self.conv_shape = self.calc_output_shape()
+
+        self.gru = nn.GRUCell(self.conv_shape, 256)
+
