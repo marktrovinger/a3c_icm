ad09cedf461d26e7426e8661c49d8a978102aedc
Mark Trovinger <mark.trovinger@gmail.com>
2022-01-25 10:59:13 -0500

    refactored cartpole

--- mp/cartpole-mp.py
+++ mp/cartpole-mp.py
@@ -2,6 +2,8 @@ import os
 import torch.multiprocessing as mp
 import gym
 
+OMS_NUM_THREADS = '1'
+
 class ParallelEnv:
     def __init__(self, env_id, num_threads):
         thread_names = [str(i) for i in range(num_threads)]
@@ -14,17 +16,25 @@ class ParallelEnv:
 # create a function that initializes and runs cartpole
 def cartpole_mp(name, env_id):
     env = gym.make(env_id)
-
-    env.reset()
+    episode, max_eps, scores = 0, 10, [] 
 
     # we don't need to run very many episodes
-    for episode in range(episodes):
-        obs, reward, done, _ = env.step(env.action_space.sample())
-        print(f'Process: {os.getpid()}, score: {reward}')
-
+    while episode < max_eps:
+        obs = env.reset()
+        score = 0
+        done = False
+        while not done:
+            action = env.action_space.sample()
+            obs_, reward, done, _ = env.step(action)
+            score += reward
+            obs = obs_
+        scores.append(score)
+        print(f'Episode: {episode}, Process: {name}, score: {score}')
+        episode += 1
     env.close()
 
 if __name__ == '__main__':
-    with Pool(5) as p:
-        p.map(cartpole_mp, [5])
-
+    mp.set_start_method('spawn')
+    env_id = 'CartPole-v0'
+    n_threads = 4
+    env = ParallelEnv(env_id=env_id, num_threads=n_threads)
