ed49579e99df2a3ee8514e5685f6c5865a6864b9
Mark Trovinger <mark.trovinger@gmail.com>
2022-01-31 10:05:13 -0500

    finished forward, and fixed stupid error

--- a3c/actor_critic.py
+++ a3c/actor_critic.py
@@ -17,7 +17,7 @@ class ActorCritic(nn.Module):
 
         self.conv4 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1)
 
-        self.conv_shape = self.calc_output_shape()
+        self.conv_shape = self.calculate_conv_output(input_dims)
 
         self.gru = nn.GRUCell(self.conv_shape, 256)
         
@@ -25,27 +25,34 @@ class ActorCritic(nn.Module):
         self.pi = nn.Linear(256, n_actions)
         self.v = nn.Linear(256, 1)
 
+    def calculate_conv_output(self, input_dims):
+        state = T.zeros(1, *input_dims)
+        dimensions = self.conv1(state)
+        dimensions = self.conv2(dimensions)
+        dimensions = self.conv3(dimensions)
+        dimensions = self.conv4(dimensions)
+        return int(np.product(dimensions.size()))
 
-def calculate_conv_output(self, input_dims):
-    state = T.zeros(1, *input_dims)
-    dimensions = self.conv1(state)
-    dimensions = self.conv2(dimensions)
-    dimensions = self.conv3(dimensions)
-    dimensions = self.conv4(dimensions)
-    return int(np.product(dimensions.size()))
+    def forward(self, state, hidden_state):
+        conv = F.elu(self.conv1(state))
+        conv = F.elu(self.conv2(conv))
+        conv = F.elu(self.conv3(conv))
+        conv = F.elu(self.conv4(conv))
 
-def forward(self, state, hidden_state):
-    conv = F.elu(self.conv1(state))
-    conv = F.elu(self.conv2(conv))
-    conv = F.elu(self.conv3(conv))
-    conv = F.elu(self.conv4(conv))
+        conv_state = conv.view((conv.size()[0], -1))
 
-    conv_state = conv.view((conv.size()[0], -1))
+        # handle our gated recurrent network
+        hidden = self.gru(conv_state, (hidden_state))
 
-    # handle our gated recurrent network
-    hidden = self.gru(conv_state, (hidden_state))
+        # send the output to our policy and value networks
+        pi = self.pi(hidden)
+        v = self.v(hidden)
 
-    # send the output to our policy and value networks
-    pi = self.pi(hidden)
-    v = self.v(hidden)
+        probs = T.softmax(pi, dim=1)
+        dist = Categorical(probs)
+        action = dist.sample()
+        log_probs =  dist.log_prob(action)
 
+        return action.numpy()[0], v, log_probs, hidden
+
+actorcritic = ActorCritic(6, [4, 42, 42])
