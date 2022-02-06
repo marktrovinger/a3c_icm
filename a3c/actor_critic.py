1558d6a51aaae678c9c0b09a95a2c81a5eba7c37
Mark Trovinger <mark.trovinger@gmail.com>
2022-02-06 18:20:31 -0500

    fixed calc_R and started calc cost functionality

--- a3c/actor_critic.py
+++ a3c/actor_critic.py
@@ -9,7 +9,7 @@ class ActorCritic(nn.Module):
     def __init__(self, n_actions, input_dims, gamma=0.99, beta=0.01, hidden_units=256):
         
         super(ActorCritic, self).__init__()
-
+        self.gamma = gamma
         self.conv1 = nn.Conv2d(input_dims[0], 32, kernel_size=(3,3), stride=2,
                 padding=1)
         self.conv2 = nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1)
@@ -54,5 +54,35 @@ class ActorCritic(nn.Module):
         log_probs =  dist.log_prob(action)
 
         return action.numpy()[0], v, log_probs, hidden
+    
+    def calcR(self, done, reward, values):
+        values = T.cat(values).squeeze()
+    
+        # if we have a batch of states
+        if len(values.size()) == 1:
+            R = values[-1] * (1-int(done))
+        # or if we have a single value
+        elif len(values.size()) == 0:
+            R = values * (1-int(done))
 
-actorcritic = ActorCritic(6, [4, 42, 42])
+        # calculate our batch returns, reverse the list and convert to tensor
+        
+        batch_returns = []
+        
+        for r in reward[::-1]:
+           R = r + self.gamma * R
+           batch_returns.append(reward)
+        batch_returns.reverse()
+        batch_returns = T.tensor(batch_returns, 
+                                dtype=T.float).reshape(values.size())
+        return batch_returns
+
+    def calc_cost(self, state_, hx, done, rewards, values, log_probs):
+        # calc returns
+        R = self.calcR(done, rewards, values)
+
+        next_v = T.zeros(1,1) if done else self.forward(T.tensor([state_],
+                                                        dtype=T.float), hx)[1]
+        values.append(next_v.detach())
+        values = T.cat(values).squeeze()
+
