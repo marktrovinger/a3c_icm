a013decee217045155990c867d2b8a22eacc8efa
Mark Trovinger <mark.trovinger@gmail.com>
2022-03-17 08:35:23 -0400

    finished repeataction and started preprocess frame

--- wrappers/wrappers.py
+++ wrappers/wrappers.py
@@ -8,8 +8,30 @@ class RepeatAction(gym.Wrapper):
         self.first_first = fire_first
 
     def step(self, action):
-        pass
+        total_reward = 0
+        done = False
+        state = None
+        info = None
+        for i in range(self.repeat):
+            obs, reward, done, info = self.env.step(action)
+            state = obs
+            info = info
+            total_reward = total_reward + reward
+            if done:
+                break
+        return state, total_reward, done, info
+
+    def reset(self):
+        state = self.env.reset()
+        if self.first_first:
+            # we need to pass an action
+            state = self.env.step()
+        return state 
 
 
 
-class PreprocessFrame:
+class PreprocessFrame(gym.ObservationWrapper):
+    def __init__(self, env, new_shape):
+        super(PreprocessFrame, self).__init__(env)
+        self.env = env
+        self.shape = new_shape
