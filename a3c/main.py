3c9b84eb9ea4e335a54105b6aac9de97b0dab63f
Mark Trovinger <mark.trovinger@gmail.com>
2022-05-05 16:21:40 -0400

    changed parallelenv to match new implementation

--- a3c/main.py
+++ a3c/main.py
@@ -1,8 +1,14 @@
+from re import I
+from numpy import unsignedinteger
 from parallel_env import ParallelEnv
 import torch.multiprocessing as mp
 
 if __name__ == '__main__':
     mp.set_start_method('spawn')
-    env_id = 'CartPole-v0'
-    n_threads = 4
-    env = ParallelEnv(env_id=env_id, num_threads=n_threads)
+    env_id = 'Pong-v0'
+    # 6 for pong, 4 for cartpole
+    n_threads = 6
+    input_shape = (42,42,1)
+    n_actions = 6
+    global_idx = mp.Value('i',0)
+    env = ParallelEnv(n_actions=n_actions,input_shape=input_shape, global_idx=global_idx,env_id=env_id, num_threads=n_threads)
