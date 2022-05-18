fc07a80b1358f9e9b2bb1c49716bd6ca2a0a726f
Mark Trovinger <mark.trovinger@gmail.com>
2022-05-18 08:36:32 -0400

    added plotting code

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,12 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_learning_curve(x, scores, filename):
+    running_average = np.zeros(len(scores))
+
+    for i in range(len(running_average)):
+        running_average[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_average)
+    plt.title('Running Average of Previous 100 Games')
+    plt.savefig(filename)
