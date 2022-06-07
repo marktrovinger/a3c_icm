150a8c7359cbef40fdd5b5a9529549ea2942a238
Mark Trovinger <mark.trovinger@gmail.com>
2022-06-07 13:51:37 -0400

    imported plot_learning_curve and fixed variable passed to make_env

--- a3c/worker.py
+++ a3c/worker.py
@@ -3,6 +3,7 @@ import gym
 from actor_critic import ActorCritic
 from wrappers.wrappers import make_env
 from memory import Memory
+from utils.utils import plot_learning_curve
 import torch as T
 
 def worker(name, env_id, global_agent, optimizer, global_idx, n_actions, input_shape, n_threads):
@@ -16,7 +17,7 @@ def worker(name, env_id, global_agent, optimizer, global_idx, n_actions, input_s
     # swap channels in our input
     frame_buffer = [input_shape[1], input_shape[2], 1]
     # create the env, using make_env function
-    env = make_env(env_id, shape = frame_buffer)
+    env = make_env(env_id, new_shape = frame_buffer)
 
     # add time_steps to variables, increase max_eps
     episode, max_eps, scores = 0, 10, []
@@ -49,12 +50,23 @@ def worker(name, env_id, global_agent, optimizer, global_idx, n_actions, input_s
                 # detach hx
                 hx = hx.detach_()
                 # clip gradients
-            # sync local with global
-            # step optimizer
-            # clear memory
+                loss.backward()
+                T.nn.utils.clip_grad_norm_(local_agent.parameters(), 40)
+                # sync local with global
+                for local_param, global_param in zip(local_agent.parameters(), global_agent.parameters()):
+                    global_param._grad = local_param.grad
+                # step optimizer
+                optimizer.step()
+                local_agent.load_state_dict(global_agent.state_dict())
+                # clear memory
+                memory.clear_memory()
         # increment episode
-        # score calculations
-        scores.append(score)
-        print(f'Episode: {episode}, Process: {name}, score: {score}')
         episode += 1
+        if name == '1':
+            scores.append(score)
+            print(f'Episode: {episode}, Process: {name}, score: {score}')
+        # score calculations
+        if name == '1':
+            x = [z for z in range(episode)]
+            plot_learning_curve()
     env.close()
