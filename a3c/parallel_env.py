652c84ec488cc1c9f441a394fc4e1749d63d98b5
Mark Trovinger <mark.trovinger@gmail.com>
2022-06-07 13:50:49 -0400

    passed correct variables to worker

--- a3c/parallel_env.py
+++ a3c/parallel_env.py
@@ -1,16 +1,17 @@
 import torch.multiprocessing as mp
-import worker
+from worker import worker
 from actor_critic import ActorCritic
 from shared_adam import SharedAdam
 
 class ParallelEnv:
-    def __init__(self, n_actions, input_shape, global_idx, worker, env_id, num_threads):
+    def __init__(self, n_actions, input_shape, global_idx, env_id, num_threads):
         thread_names = [str(i) for i in range(num_threads)]
         global_agent = ActorCritic(n_actions=n_actions, input_dims=input_shape)
         global_agent.share_memory()
-        global_optimizer = SharedAdam(global_agent.parameters)
+        global_optimizer = SharedAdam(global_agent.parameters(), lr=1e-4)
 
-        self.procs = [mp.Process(target=worker.worker, args=(name, env_id, global_agent, global_optimizer, global_idx))
+        self.procs = [mp.Process(target=worker, args=(thread_names, env_id, global_agent, global_optimizer, global_idx, 
+                                                    n_actions, input_shape, num_threads))
                       for name in thread_names]
         [p.start() for p in self.procs]
         [p.join() for p in self.procs]
